f213b18de775c510efbcb4ce5fe0283f62ea60fa
BlueMoon55 <mskn55@gmail.com>
2018-08-15 17:01:07 +0900

    20180815e

--- gyakubiki/08_04/yahoo_headline.py
+++ gyakubiki/08_04/yahoo_headline.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-xml = requests.get('http://news.yahoo.co.jp/pickup/science/rss.xml')
+xml = requests.get('http://news.yahoo.co.jp/pickup/science/rss.xml', verify=False)
 soup = BeautifulSoup(xml.text, 'html.parser')
 for news in soup.findAll('item'):
-    print(news.title.string)
+    print(news.title.string, '\n', news)
